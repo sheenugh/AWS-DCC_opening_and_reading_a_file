044d498cfc773d9731512413c78afb0ea69616a8
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-18 21:24:28 +0800

    pseudo code

---
+++ opening_and_reading_file.py
@@ -0,0 +1,9 @@
+# Write a short Python program that opens a file named "example.txt" and prints its content.
+
+# NOTE: only use built-in libraries of python (do not use pandas etc.)
+
+# ========== PSEUDO CODE ==========
+# - Open Function
+# - Variable Stored
+# - Using File Function
+# - Code for file close
